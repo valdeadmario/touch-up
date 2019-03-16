dc7d7fc0d793e918cce24ab31af0708953c141e3
Valdeadmario <vladimir.vstup@gmail.com>
2019-03-16 20:18:12 +0200

    Update item component

--- src/components/item/item.js
+++ src/components/item/item.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+import defaultParams from '../params';
 
-const Item = () => {
-  return(<div>Item...</div>)
+export default class Item extends Component{
+  state = {
+    fired: false
+  };
+
+  tooLate = setTimeout(
+    () => {
+      this.setState({fired: true})
+      this.props.targetFired()
+    },
+    defaultParams.periodForClickMsec
+  )
+
+  clearTimeout = () => {
+    clearTimeout(this.tooLate)
+    return true;
+  }
+
+  render() {
+    return (
+      <div
+        className='item'
+        onClick={
+          () => {
+            !this.state.fired
+            && this.clearTimeout()
+            && this.props.clickHandler(this.props.id)
+          }
+        }
+        />
+    )
+  }
 }
-
-export default Item;
