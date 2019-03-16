c3021210a9cca4602b696a686b757d220c7c746f
Valdeadmario <vladimir.vstup@gmail.com>
2019-03-16 20:39:27 +0200

    Create ItemGeneration, clickTarget and firedTarget functions

--- src/components/game/game.js
+++ src/components/game/game.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { defaultParams } from "../params";
+import Item from "../item/item";
+
 
 let gameParams = {...defaultParams}
 
@@ -14,6 +16,48 @@ export default class Game extends Component{
   state = {
     ...initialGameState(),
     start: false
+  };
+
+  targetGenerate = ({probability, periodMsec}) => {
+    return setInterval(() => {
+      if (Math.random() * 100 <= probability) {
+        const x = Math.random() * (gameParams.fieldWidth - gameParams.targetWidth)
+        const y = Math.random() * (gameParams.fieldHeight - gameParams.targetHeight)
+        this.setState({
+          targets: [
+            ...this.state.targets,
+            <Item
+              id = {this.state.targetsCnt}
+              key = {this.state.targetsCnt}
+              coordinate = {{x, y}}
+              clickHandler = {this.clickTarget}
+              targetFired = {this.targetFired}
+            />
+          ],
+          targetsCnt: ++this.state.targetsCnt
+        })
+      }
+    }, periodMsec)
   }
 
+  clickTarget = (id) => {
+    this.setState(({ targets }) => {
+      const idx = targets.findIndex((item) => item.id === id);
+
+      const newArr = [
+        ...targets.slice(0,idx),
+        ...targets.slice(idx+1)
+      ];
+      return {
+        targets: newArr
+      }
+    });
+  };
+
+  targetFired = () => {
+    const life = this.state.life > 0
+      ? this.state.life - 1
+      : 0
+    this.setState({life: life})
+  }
 }
