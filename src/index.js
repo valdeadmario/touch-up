b6dbabe443e32c4ff1fdd7f403eb2ba074dae966
Valdeadmario <vladimir.vstup@gmail.com>
2019-03-20 18:35:49 +0200

    Create redux tree and redux store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import './index.scss';
+import { Provider } from 'react-redux';
 
 import App from './components/app';
+import store from "./store";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.scss';
+import ErrorBoundary from "./components/error-boundary";
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
+  </Provider>,
+  document.getElementById('root'));
